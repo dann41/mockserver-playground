a81f58740c8cd752a4b8a9a5295ffbec97b7f0a5
Alex González Lacasa <alejandro.gonzalez@adevinta.com>
2020-11-05 13:09:56 +0100

    automotive

--- start-servers.bat
+++ start-servers.bat
@@ -1,9 +1,15 @@
-#!/bin/bash
-
 cd addresses
 docker-compose up -d
 cd ../customers
 docker-compose up -d
 cd ../orders
 docker-compose up -d
+cd ..
+cd automotive-makes
+cd coches
+docker-compose up -d
+cd ..
+cd motos
+docker-compose up -d
+cd ..
 cd ..
