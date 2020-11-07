e4096d75347781aec439f2a19b22f01d512aa2ef
Daniel Gómez <daniels26@gmail.com>
2020-11-07 21:11:06 +0100

    Add main workflow (#2)
    * Add workflow and improve testing

--- stop-servers.sh
+++ stop-servers.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-cd addresses
 docker-compose down
-cd ../customers
-docker-compose down
-cd ../orders
-docker-compose down
-cd ..
-cd automotive-makes
-cd coches
-docker-compose down
-cd ..
-cd motos
-docker-compose down
-cd ..
-cd ..
