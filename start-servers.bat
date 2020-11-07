1161793044b7677139f1f0ba65ad6c788bf149ce
Daniel Gomez <daniels26@gmail.com>
2020-11-07 20:03:31 +0100

    Unify docker-compose files

--- start-servers.bat
+++ start-servers.bat
@@ -1,15 +1,1 @@
-cd addresses
 docker-compose up -d
-cd ../customers
-docker-compose up -d
-cd ../orders
-docker-compose up -d
-cd ..
-cd automotive-makes
-cd coches
-docker-compose up -d
-cd ..
-cd motos
-docker-compose up -d
-cd ..
-cd ..
--- stop-servers.bat
+++ stop-servers.bat
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-cd addresses
 docker-compose down
-cd ../customers
-docker-compose down
-cd ../orders
-docker-compose down
-cd ..
-cd automotive-makes
-cd coches
-docker-compose down
-cd ..
-cd motos
-docker-compose down
-cd ..
-cd ..
